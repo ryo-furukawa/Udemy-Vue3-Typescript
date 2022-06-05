4c21dc547892656b0f4277ad9faf520fc3b75e72
ryo-furukawa <furu3548@gmail.com>
2022-06-05 23:00:05 +0900

    section7

---
+++ src/components/Tweet.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import TweetPostForm from './TweetPostForm.vue';
+import TweetList from './TweetList.vue';
+const tweets = ref([{
+  id: 0,
+  description: 'Hello, World'!
+}, 
+{
+  id: 1,
+  description: 'This is the second tweet'!
+},])
+
+const inputtingDescription = ref<string>('')
+
+const postTweet = (description: string) => {
+  const tweet = { id:Math.random(), description }
+  tweets.value.push(tweet)
+}
+
+const deleteTweet = (id: number) => {
+  tweets.value = tweets.value.filter(t => t.id !== id)
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Tweetter</h1>
+      <TweetPostForm @post-tweet="postTweet" />
+    <div class="tweet-container">
+      <p v-if="tweets.length <= 0">No tweets have been added</p>
+      <ul v-else>
+        <TweetList :tweets="tweets" @delete-tweet="deleteTweet"/>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+input {
+  margin-bottom: 16px;
+}
+</style>
---
+++ src/components/TweetList.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+type Tweet = {
+    id: number,
+    description: string,
+}
+
+type Props = {
+    tweets: Tweet[]
+}
+
+defineProps<Props>()
+
+const emit = defineEmits(['delete-tweet'])
+const deleteTweet = (id: number) => {
+    emit('delete-tweet', id)
+}
+</script>
+
+
+<template>
+    <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
+        <span>{{ tweet.description }}</span>
+        <button class="delete-button" @click="deleteTweet(tweet.id)">delete</button>
+    </li>
+</template>
+
+<style scoped>
+.tweet-list {
+    list-style: none;
+    margin-bottom: 12px;
+    border-radius: 4px;
+    font-size: 12px;
+    display: flex;
+    justify-content: space-between;
+    background-color: rgb(204, 219, 233);
+    padding: 8px 20px;
+    width: 300px;
+}
+
+.delete-button {
+    color: #fff;
+    font-weight: bold;
+    background-color: #e64c2d;
+    border-radius: 2px;
+    border: none;
+    width: 60px;
+    height: 22px;
+}
+
+.delete-button:hover {
+    background-color: #ef2f09;
+}
+</style>
